26d4ebd8e1bb27802c376c277be24a29e81a952d
wikto <wiktor.labno@edu.uekat.pl>
2024-02-21 17:08:59 +0100

    zd 1-2czIII

--- cw1.cpp
+++ cw1.cpp
@@ -14,19 +14,17 @@ int main()
     int len = sizeof(grades) / sizeof(grades[0]);
     int sumaM = 0;
     int sumaF = 0;
-// printf("%d", sizeof(grades[0])/sizeof(grades[0][0]))  //liczba kolumn
+    //printf("%d", sizeof(grades[0])/sizeof(grades[0][0]));  //liczba kolumn
 
     for (int i = 0; i < len; i++)
     {
-        for (int j = 0; i < len; i++)
-        {
             sumaM += grades[i][0];
             sumaF += grades[i][1];
-        }
     }
 
     printf("Srednia z matematyki: %.2f\n", sumaM / (float)(len ));
     printf("Srednia z fizyki : %.2f\n", sumaF / (float)(len ));
+    printf("%d",len);
 
 
     return 0;
