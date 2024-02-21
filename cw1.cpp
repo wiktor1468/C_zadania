cc23e607fb254de092482a3c915dc20d5cfe434c
wikto <wiktor.labno@edu.uekat.pl>
2024-02-21 17:00:49 +0100

    zd 1-2

---
+++ cw1.cpp
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+int main()
+{
+    //zd2
+    int grades[5][2] = {
+            {5, 6},
+            {4, 2},
+            {3, 5},
+            {3, 5},
+            {5, 6}
+    };
+    int len = sizeof(grades) / sizeof(grades[0]);
+    int sumaM = 0;
+    int sumaF = 0;
+// printf("%d", sizeof(grades[0])/sizeof(grades[0][0]))  //liczba kolumn
+
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; i < len; i++)  // Uwaga: To powinno być "j < len" zamiast "i < len"
+        {
+            sumaM += grades[i][0];
+            sumaF += grades[i][1];
+        }
+    }
+
+    printf("Srednia z matematyki: %.2f\n", sumaM / (float)(len ));
+    printf("Srednia z fizyki : %.2f\n", sumaF / (float)(len ));
+
+
+    return 0;
+}
